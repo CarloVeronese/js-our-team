171f70d36efbcf6e7821c1fe9f9e0d9b398483bb
CarloVeronese <carlo.veronese97@gmail.com>
2023-10-18 13:54:59 +0200

    create - team members array

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const teamMembers = [
+    {
+        name:'Wayne Barnett',
+        position:'Founder & CEO',
+        picture: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name:'Angela Caroll',
+        position:'Chief Editor',
+        picture:'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name:'Walter Gordon',
+        position:'Office Manager',
+        picture:'walter-gordon-office-manager.jpg',
+    },
+    {
+        name:'Angela Lopez',
+        position:'Social Media Manager',
+        picture:'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name:'Scott Estrada',
+        position:'Developer',
+        picture:'scott-estrada-developer.jpg',
+    },
+    {
+        name:'Barbara Ramos',
+        position:'Graphic Designer',
+        picture:'barbara-ramos-graphic-designer.jpg',
+    }
+]
+
+console.log(teamMembers)
